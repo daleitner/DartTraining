893c390ef782e95cf11f1bc883dee139c4b1d5e6
Daniel Leitner <dmcleite@hotmail.com>
2017-05-31 15:37:20 +0200

    added approvaltest project. added pictures for gui

---
+++ DartTraining/ApprovalTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApprovalTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
